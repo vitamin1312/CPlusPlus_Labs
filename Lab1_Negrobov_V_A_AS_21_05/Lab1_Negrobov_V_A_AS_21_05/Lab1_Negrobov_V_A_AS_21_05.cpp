ff8c50120fe4f42f15e9bf4534bf883fc02bb60f
vitamin1312 <107752653+vitamin1312@users.noreply.github.com>
2022-09-13 11:45:03 +0300

    Создание структур данных

---
+++ Lab1_Negrobov_V_A_AS_21_05/Lab1_Negrobov_V_A_AS_21_05/Lab1_Negrobov_V_A_AS_21_05.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+struct pipe {
+	float len;
+	float diameter;
+	bool in_repairing;
+};
+
+
+struct compr_station {
+	string name;
+	int num_workshops;
+	int num_run_workshops;
+	float efficiency;
+};
+
+
+int main()
+{
+    cout << "Hello World!\n";
+}
